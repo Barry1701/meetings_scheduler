3861f186b4c866a8e3e596a584237c914025c5b1
Bartosz Stanczuk <synergy170185@gmail.com>
2024-06-10 15:36:21 +0000

    Add couple of testing functions in view.py and import them in urls

--- meetings_scheduler/website/views.py
+++ meetings_scheduler/website/views.py
@@ -1,3 +1,13 @@
+from datetime import datetime
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to the Meeting Scheduler!')
+
+def date(request):
+    return HttpResponse('Ta Strona zostala zaladowana ' + str(datetime.now()))
+
+def about(request):
+    return HttpResponse('Experienced security professional with over a decade of industry expertise, currently completing a full stack developer course. Skilled in access control management, surveillance oversight, and incident response. Proven track record of delivering exceptional customer service and driving continuous improvement. Eager to leverage technical proficiency and adaptability in dynamic environments.')
