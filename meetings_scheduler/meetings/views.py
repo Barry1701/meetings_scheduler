a6793894c87963892da878949e2e59bd3c5dae6d
Bartosz Stanczuk <synergy170185@gmail.com>
2024-06-13 16:29:59 +0000

    Add urls.py to my meeting application for best practice

--- meetings_scheduler/meetings/views.py
+++ meetings_scheduler/meetings/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from .models import Meeting
+from .models import Meeting, Room
 
 # Create your views here.
 
 def detail(request, id):
     meeting = Meeting.objects.get(pk=id)
     return render(request, 'meetings/detail.html', {'meeting' : meeting})
+
+
+def rooms_list(request):
+    return render(request,'meetings/rooms_list.html',
+    {'rooms': Room.objects.all()})
+
